0fc3ae707a015bcdc17cb1ad99526f8ee54f0860
Jonathan Woolley <jwoolley077@gmail.com>
2018-01-18 21:05:42 +1100

    #GKF-78 - social buttons at the top are intermittent

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderRoutes } from './header-routing.module';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { LocalStorageService } from 'ngx-webstorage';
 import { AuthService } from '../../../core/services/auth.service';
 import { Observable } from 'rxjs/Observable';
@@ -14,6 +14,7 @@ import { debounce } from 'rxjs/operators/debounce';
 export class HeaderComponent implements OnInit {
   public menuItems: any[];
   public brandMenu: any;
+  private twitter: any;
   isCollapsed = true;
   isAuthenticated = false;
 
@@ -25,6 +26,7 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initTwitterWidget();
     this.menuItems = HeaderRoutes;
     const token = this.localStorage.retrieve('token');
     if (token) {
@@ -57,4 +59,34 @@ export class HeaderComponent implements OnInit {
     return this.isCollapsed ? '☰' : '✖';
   }
 
+  initTwitterWidget() {
+    this.twitter = this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        (<any>window).twttr = (function (d, s, id) {
+          // tslint:disable-next-line:prefer-const
+          let js: any, fjs = d.getElementsByTagName(s)[0],
+              // tslint:disable-next-line:prefer-const
+              t = (<any>window).twttr || {};
+          // tslint:disable-next-line:curly
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = 'https://platform.twitter.com/widgets.js';
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function (f: any) {
+              t._e.push(f);
+          };
+
+          return t;
+        }(document, 'script', 'twitter-wjs'));
+
+        // tslint:disable-next-line:curly
+        if ((<any>window).twttr.ready())
+          (<any>window).twttr.widgets.load();
+
+      }
+    });
+  }
 }
