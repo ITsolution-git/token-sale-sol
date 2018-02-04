91185036c18d329531dc45ea71007a2d8bde7931
Jonathan Woolley <jwoolley077@gmail.com>
2018-02-05 03:13:22 +1100

    #GKF-135 - [Mobile] Press #Created contact form

--- src/app/pages/press/press.component.ts
+++ src/app/pages/press/press.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+declare const $: any;
 
 @Component({
   selector: 'app-press',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PressComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  isMobile = false;
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
+  ngOnInit() {
+    this.isMobile = this.isMobileView();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+     this.isMobile = this.isMobileView();
+  }
+
+  isMobileView() {
+    if ($(window).width() > 425) {
+        return false;
+    }
+    return true;
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    const {name, email, message} = this.form.value;
+    const date = Date();
+    const html = `
+      <div>From: ${name}</div>
+      <div>Email: <a href="mailto:${email}">${email}</a></div>
+      <div>Date: ${date}</div>
+      <div>Message: ${message}</div>
+    `;
+    const formRequest = { name, email, message, date, html };
+    // TODO: Need Email API to send form data
+    this.form.reset();
+  }
 }
--- src/app/pages/press/press.module.ts
+++ src/app/pages/press/press.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PressComponent } from './press.component';
 import { PressRoutingModule } from './press-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
     CommonModule,
     PressRoutingModule
   ],
