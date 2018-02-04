6a9f622ad91843019d12678cbcad0ddafcc3e9ab
Jonathan Woolley <jwoolley077@gmail.com>
2018-02-04 11:37:45 +1100

    #GKF-138 - [Mobile] Item Details

--- src/app/pages/item-detail/item-detail.module.ts
+++ src/app/pages/item-detail/item-detail.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ItemDetailComponent } from './item-detail.component';
 import { ItemDetailRoutingModule } from './item-detail-routing.module';
 import { ComponentsModule } from '../../shared/components/components.module';
+import {ShareModule} from 'ng2share/share.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ItemDetailRoutingModule,
     ComponentsModule,
+    ShareModule
   ],
   declarations: [ItemDetailComponent]
 })
