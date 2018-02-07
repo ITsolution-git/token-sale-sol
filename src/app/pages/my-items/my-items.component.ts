6903e993889be89b5a36bfa337dbdf288749b895
Jonathan Woolley <jwoolley077@gmail.com>
2018-02-08 08:30:28 +1100

    #GKF-164 - My Inventory Page Layout

---
+++ src/app/pages/my-items/my-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-items',
+  templateUrl: './my-items.component.html',
+  styleUrls: ['./my-items.component.scss']
+})
+export class MyItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
