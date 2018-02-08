8d788a8bf9dc7a54816c2f27b55d150a05861b90
itsolution-git <roy.smith0820@gmail.com>
2018-02-08 11:46:36 -0500

    merge develop

--- src/app/shared/components/header/header-routing.module.ts
+++ src/app/shared/components/header/header-routing.module.ts
@@ -2,7 +2,8 @@ import { RouteInfo } from './header.metadata';
 
 export const HeaderRoutes: RouteInfo[] = [
   { path: '', title: 'Home', fragment: 'home' },
-  { path: '/buy-gzr', title: 'Get GZR', fragment: ''}
+  { path: '/buy-gzr', title: 'Get GZR', fragment: ''},
+  { path: '/my-items', title: 'My Profile', fragment: ''}
 ];
 
 export const MobileHeaderRoutes: RouteInfo[] = [
