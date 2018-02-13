105742e496f9b711502efd464399141a6fc6f43f
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-02-14 00:04:46 +0100

    Preparing opening treasure modal

---
+++ src/app/shared/components/opening-treasure-modal/opening-treasure-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TreasureModalComponent } from './treasure-modal.component';
+
+describe('TreasureModalComponent', () => {
+  let component: TreasureModalComponent;
+  let fixture: ComponentFixture<TreasureModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TreasureModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TreasureModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
