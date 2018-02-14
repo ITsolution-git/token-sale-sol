3e808cd883c40d2a157ea3611f0901d9baecb3f0
Jonathan Woolley <jwoolley077@gmail.com>
2018-02-14 18:25:54 +0100

    GKF-241 - Clickid for Buy GZR button in the 3 homepage slider images: buygzr-slider

--- src/app/shared/components/hero-slider/hero-slider.module.ts
+++ src/app/shared/components/hero-slider/hero-slider.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SlickModule } from 'ngx-slick';
 import { HeroSliderComponent } from './hero-slider.component';
+import { Angulartics2Module } from 'angulartics2';
 
 @NgModule({
   imports: [
       RouterModule,
       CommonModule,
       SlickModule.forRoot(),
+      Angulartics2Module
     ],
   declarations: [ HeroSliderComponent ],
   exports: [ HeroSliderComponent ]
